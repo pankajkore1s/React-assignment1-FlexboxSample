0b914a100ce38fb9e3ca4ab2df7417e7ff79e98b
pankaj kore <korep95@gmail.com>
2022-05-02 00:29:42 +0530

    [pankaj]created sample flexbox

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <div class="wrapper">
+          <header class="header">
+            <h1>Header</h1>
+          </header>
+          <article class="main">
+            <p>HTML paragraph or HTML p tag is used to define a paragraph in a webpage. Let's take a simple example to see how it work. It is a notable point that a browser itself add an empty line before and after a paragraph. If you put a lot of spaces inside the HTML p tag, browser removes extra spaces and extra line while displaying the page. The browser counts.
+           </p>
+            </article>
+            <aside class="aside aside1">
+              <h1>Aside1</h1>
+              </aside>
+            <aside class="aside aside2">
+              <h1>Aside2</h1>
+              </aside>
+              <footer class="footer">
+              <h1>Footer
+                </h1>
+              </footer>  
+        </div>
+      </body>
     </div>
   );
 }
